8156a1ec91e8665eceb86d8fa25f482a6ab71a5b
NorPeK <norpekbusiness@hotmail.com>
2024-11-12 12:26:05 +0300

    startup card implemented

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import StartupCard from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
 
 export default async function Home({ searchParams }: {
@@ -5,6 +6,16 @@ export default async function Home({ searchParams }: {
 }) {
   const query = (await searchParams).query;
 
+  const posts = [{
+    _createdAt: new Date(),
+    views: 55,
+    author: { id: 1 , name: "NorPeK"},
+    _id: 1,
+    description: 'This is a description',
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5vYvRzvvp5S82meHmleXnmmkHVciu90SgIg&s",
+    category: "Robots",
+    title: "We Robots",
+  }];
 
   return (
     <>
@@ -17,6 +28,23 @@ export default async function Home({ searchParams }: {
         </p>
 
         <SearchForm query={query}/>
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          { posts?.length > 0 ? (
+            posts.map((post: StartupCardType) => (
+              <StartupCard key={post?._id} post={post}/>
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
+
       </section>
     </>
   );
