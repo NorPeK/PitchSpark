42fe9f9e06fce4be700d79f4039901d3a5e381ef
NorPeK <norpekbusiness@hotmail.com>
2024-11-13 10:36:21 +0300

    Cache and live API

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,8 @@
 import StartupCard, {StartupTypeCard} from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
-import { client } from "@/sanity/lib/client";
+//import { client } from "@/sanity/lib/client";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
+import { sanityFetch , SanityLive} from "@/sanity/lib/live";
 
 
 export default async function Home({ searchParams }: {
@@ -9,7 +10,9 @@ export default async function Home({ searchParams }: {
 }) {
   const query = (await searchParams).query;
 
-  const posts = await client.fetch(STARTUPS_QUERY);
+  //const posts = await client.fetch(STARTUPS_QUERY);
+
+  const {data: posts} = await sanityFetch({query: STARTUPS_QUERY});
 
   console.log(JSON.stringify(posts, null, 2))
 
@@ -44,6 +47,7 @@ export default async function Home({ searchParams }: {
         </ul>
 
       </section>
+      <SanityLive/>
     </>
   );
 }
